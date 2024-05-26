a5b2ed334098d79cd571728ef64080465838bcf9
Aiden Ma <52476917+mjykxz17@users.noreply.github.com>
2024-05-27 01:50:17 +0800

    Update 0.0.1
    Updated menu bar and implemented API for pulling upcoming movies

--- lib/front_page.dart
+++ lib/front_page.dart
@@ -1,51 +1,145 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'services/movie_service.dart';
 
-class FrontPage extends StatelessWidget {
+class FrontPage extends StatefulWidget {
+  @override
+  _FrontPageState createState() => _FrontPageState();
+}
+
+class _FrontPageState extends State<FrontPage> {
+  final MovieService _movieService = MovieService();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset(
-              'images/logo.png', // Path to your logo image
-              width: 500,
-            ),
-            Text(
-              'Welcome!',
-              style: TextStyle(
-                color: Colors.yellow,
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 20),
-            GestureDetector(
-              onTap: () {
-                FirebaseAuth.instance.signOut();
-                Navigator.pushNamed(context, "/login");
-              },
-              child: Container(
-                width: 100,
-                height: 45,
-                decoration: BoxDecoration(
-                  color: Colors.yellow,
-                  borderRadius: BorderRadius.circular(18),
-                ),
-                child: Center(
-                  child: Text(
-                    "Logout",
-                    style: TextStyle(
-                        color: Colors.black, fontWeight: FontWeight.bold),
+      body: FutureBuilder<List<dynamic>>(
+        future: _movieService.fetchUpcomingMovies(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(
+                child: Text('Error: ${snapshot.error}',
+                    style: TextStyle(color: Colors.white)));
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data?.length ?? 0,
+              itemBuilder: (context, index) {
+                var movie = snapshot.data![index];
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    color: Colors.grey[850],
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Image.network(
+                          'https://image.tmdb.org/t/p/w500${movie['poster_path']}',
+                          width: 150,
+                          height: 225,
+                          fit: BoxFit.cover,
+                        ),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  movie['title'],
+                                  style: TextStyle(
+                                    color: Colors.yellow,
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(height: 10),
+                                Text(
+                                  'Release Date: ${movie['release_date']}',
+                                  style: TextStyle(
+                                    color: Colors.white70,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                                SizedBox(height: 10),
+                                Text(
+                                  movie['overview'],
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 14,
+                                  ),
+                                  maxLines: 5,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ),
-            ),
-          ],
-        ),
+                );
+              },
+            );
+          }
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.black,
+        selectedItemColor: Colors.yellow,
+        unselectedItemColor: Colors.grey,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.home),
+            label: 'Home',
+            tooltip: 'Home Page',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.qrcode),
+            label: 'Scan',
+            tooltip: 'Scan Ticket',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.comments),
+            label: 'Forum',
+            tooltip: 'Forum',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.cog),
+            label: 'Settings',
+            tooltip: 'Settings',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.signOutAlt, color: Colors.red),
+            label: 'Logout',
+            tooltip: 'Logout',
+          ),
+        ],
+        onTap: (index) {
+          switch (index) {
+            case 0:
+              Navigator.pushNamed(context, '/home');
+              break;
+            case 1:
+              Navigator.pushNamed(context, '/scan');
+              break;
+            case 2:
+              Navigator.pushNamed(context, '/forum');
+              break;
+            case 3:
+              Navigator.pushNamed(context, '/settings');
+              break;
+            case 4:
+              FirebaseAuth.instance.signOut();
+              Navigator.pushNamed(context, '/login');
+              break;
+          }
+        },
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'front_page.dart';
 import 'forgot_password_page.dart';
 import 'email_sent_page.dart';
 import 'features/app/user_auth/presentation/pages/sign_up_page.dart';
+import 'services/movie_service.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -39,7 +40,71 @@ class MyApp extends StatelessWidget {
         '/forgot_password': (context) => ForgotPasswordPage(),
         '/email_sent': (context) => EmailSentPage(),
         '/signup': (context) => SignUpPage(),
+        '/upcoming_movies': (context) => UpcomingMoviesScreen(),
+        // Define the /forum route if it's referenced somewhere in the app
+        '/forum': (context) =>
+            ForumPage(), // Assuming you have a ForumPage widget
       },
+    );
+  }
+}
+
+class UpcomingMoviesScreen extends StatefulWidget {
+  @override
+  _UpcomingMoviesScreenState createState() => _UpcomingMoviesScreenState();
+}
+
+class _UpcomingMoviesScreenState extends State<UpcomingMoviesScreen> {
+  final MovieService _movieService = MovieService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text('Upcoming Movies'),
+        backgroundColor: Colors.black,
+      ),
+      body: FutureBuilder<List<dynamic>>(
+        future: _movieService.fetchUpcomingMovies(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data?.length ?? 0,
+              itemBuilder: (context, index) {
+                var movie = snapshot.data![index];
+                return ListTile(
+                  leading: Image.network(
+                      'https://image.tmdb.org/t/p/w92${movie['poster_path']}'),
+                  title: Text(movie['title'],
+                      style: TextStyle(color: Colors.white)),
+                  subtitle: Text(movie['release_date'],
+                      style: TextStyle(color: Colors.white70)),
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+// Define the ForumPage widget if you reference it in routes
+class ForumPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Forum'),
+      ),
+      body: Center(
+        child: Text('Forum Page Content'),
+      ),
     );
   }
 }
