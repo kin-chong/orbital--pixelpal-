68770b86dd15209c630952d0787c3e4f6f780aa2
kin chong (casey) <kinchong_low@yahoo.com>
2024-06-05 17:08:44 +0800

    Update front_page.dart

--- lib/features/app/user_auth/presentation/pages/front_page.dart
+++ lib/features/app/user_auth/presentation/pages/front_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'services/movie_service.dart';
-import 'movie_detail_page.dart'; // Import the MovieDetailPage
+import '../../../../../services/movie_service.dart';
+import '../../../../../movie_detail_page.dart'; // Import the MovieDetailPage
 
 class FrontPage extends StatefulWidget {
   const FrontPage({super.key});
