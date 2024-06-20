987570ad64733ef7516795694d441e64bcf223ff
Aiden Ma <52476917+mjykxz17@users.noreply.github.com>
2024-06-20 23:29:39 +0800

    Forum function 8 jun 2024 (#12)
    * Update
    
    1.Extracted bottom nav bar as separate module
    2.Made the logo in profile page smaller to be compatible with mobile devices
    3.Set up frontend of the forum page.
    
    * Updated firebase credentials
    
    * Forum Function
    
    1.Configured firebase backend
    2.Able to create post and seen by other users
    
    ---------
    
    Co-authored-by: casey <41713710+kin-chong@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,8 @@ import 'package:pixelpal/features/app/user_auth/presentation/pages/forgot_passwo
 import 'package:pixelpal/features/app/user_auth/presentation/pages/email_sent_page.dart';
 import 'package:pixelpal/features/app/user_auth/presentation/pages/sign_up_page.dart';
 import 'package:pixelpal/features/app/user_auth/presentation/pages/forum_page.dart';
+import 'package:pixelpal/features/app/user_auth/presentation/pages/create_post_page.dart'; // Add CreatePostPage
 //import 'package:pixelpal/features/app/user_auth/presentation/pages/scan_page.dart'; // Add ScanPage if it's not already added
-//import 'package:pixelpal/features/app/user_auth/presentation/pages/create_post_page.dart'; // Add CreatePostPage if it's not already added
 import 'services/movie_service.dart';
 
 Future<void> main() async {
@@ -54,7 +54,7 @@ class MyApp extends StatelessWidget {
         '/profile': (context) => ProfileMenu(),
         '/home': (context) => const FrontPage(), // Ensure this route is added
         //'/scan': (context) => const ScanPage(), // Ensure ScanPage is added
-        //'/createPost': (context) => const CreatePostPage(), // Ensure CreatePostPage is added
+        '/createPost': (context) => const CreatePostPage(), // Ensure CreatePostPage is added
       },
     );
   }
