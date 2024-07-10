206b9a9aa1e71ba0c7016566b3fdf15073fa6058
kin chong (casey) <kinchong_low@yahoo.com>
2024-07-10 13:02:11 +0800

    profile picture change permanently reflected

--- lib/global/common/utils.dart
+++ lib/global/common/utils.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:intl/find_locale.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:pixelpal/global/common/toast.dart';
 
-pickImage(ImageSource source) async {
+pickImage(BuildContext context, ImageSource source) async {
   final ImagePicker _imagePicker = ImagePicker();
   XFile? _file = await _imagePicker.pickImage(source: source);
   if (_file != null) {
-    return await _file.readAsBytes();
+    CroppedFile? croppedFile = await ImageCropper().cropImage(
+      sourcePath: _file.path,
+      aspectRatio: const CropAspectRatio(ratioX: 1, ratioY: 1),
+    );
+
+    if (croppedFile != null) {
+      return await croppedFile.readAsBytes();
+    } else {
+      showToast(message: 'Image cropping cancelled.');
+    }
   }
-  print('No Images Selected');
+  showToast(message: 'No Image Selected.');
 }
